fc60cbb48cce6fffd8c8401fcc101f7c364ec359
NifedZenaX <85544639+NifedZenaX@users.noreply.github.com>
2021-12-31 10:43:37 +0700

    Tower and Level Manager

---
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _towerPlace;
+    [SerializeField] private SpriteRenderer _towerHead;
+
+    [SerializeField] private int _shootPower = 1;
+    [SerializeField] private float _shootDistance = 1f;
+    [SerializeField] private float _shootDelay = 5f;
+    [SerializeField] private float _bulletSpeed = 1f;
+    [SerializeField] private float _bulletSplashRadius = 0f;
+
+    public Sprite GetTowerHeadIcon()
+    {
+        return _towerHead.sprite;
+    }
+}
